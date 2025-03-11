29bd6f4f02db76d7e22aec605f7b4619fbd8a8fe
afra <afra.meymi@code.berlin>
2025-03-11 11:09:31 +0100

    made routs and fixed some of the pages

--- app.js
+++ app.js
@@ -1,10 +1,40 @@
 import express, { response } from 'express';
+import { request } from 'http';
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true }))
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
 const port = 3000;
 
-app.get('/', (request, response) => {
-    response.send('Welcome to my Cookieshop!');
+app.get('/home', (request, response) => {
+    response.sendFile(__dirname+'/home.html');
+  })
+
+app.get('/report',(request, response) => {
+  response.sendFile(__dirname+'/report.html');
+})
+
+app.get('/track', (request, response) =>{
+  response.sendFile(__dirname + '/track.html');
+})
+
+app.get('/track/:id', (request, response) => {
+  const homeId = request.params.id;
+
+  response.send(`This is a dynamic route ${homeId}`)
+} )
+
+app.post('/feedback' ,(request, response) => {
+  console.log(request.body.email)
+  
+  response.send(`Thank you ${request.body.email}. Your feedback is important for us.`);
 })
 
 app.listen(port, () => {
